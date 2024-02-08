6e5cd4cc97e389183f73d0821f1fa17d0d682e5e
Yanick Nedderhoff <hey@yanick.xyz>
2024-02-08 21:46:37 +0100

    add deserialisation from base64

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from modules.RoaringBitmapUtil import RoaringBitmapUtil
 
+
 # below are two byte array representations of RoaringBitmaps
 
-# the byte array representation of a RoaringBitmap containing id '938c'
-SERIALISED_BITMAP_1 = bytes([58, 48, 0, 0, 1, 0, 0, 0, 51, 57, 0, 0, 16, 0, 0, 0, 99, 56])
+# the base64 representation of the byte array representation of a RoaringBitmap containing id '938c'
+SERIALISED_BITMAP_1_BASE64 = 'OjAAAAEAAAAzOQAAEAAAAGM4'
 # the byte array representation of a RoaringBitmap containing ids '938c' and '3f7e'
-SERIALISED_BITMAP_2 = bytes([58, 48, 0, 0, 2, 0, 0, 0, 102, 51, 0, 0, 51, 57, 0, 0, 24, 0, 0, 0, 26, 0, 0, 0, 101, 55, 99, 56])
+SERIALISED_BITMAP_2_BYTE_ARRAY = bytes([58, 48, 0, 0, 2, 0, 0, 0, 102, 51, 0, 0, 51, 57, 0, 0, 24, 0, 0, 0, 26, 0, 0, 0, 101, 55, 99, 56])
 
 
 def run():
-    # deserialise both byte arrays to BitMap objects
-    roaring_bitmap_1 = RoaringBitmapUtil.from_byte_array(SERIALISED_BITMAP_1)
-    roaring_bitmap_2 = RoaringBitmapUtil.from_byte_array(SERIALISED_BITMAP_2)
+    # deserialise both the base64 and the byte array to BitMap objects
+    roaring_bitmap_1 = RoaringBitmapUtil.from_base64(SERIALISED_BITMAP_1_BASE64)
+    roaring_bitmap_2 = RoaringBitmapUtil.from_byte_array(SERIALISED_BITMAP_2_BYTE_ARRAY)
 
     # bitwise AND
     intersected_bitmap = roaring_bitmap_1 & roaring_bitmap_2
@@ -24,7 +25,7 @@ def run():
     print("Deserialised ids from bitmap 1:\t" + str(string_array_1))  # ['938c']
     print("Deserialised ids from bitmap 2:\t" + str(string_array_2))  # ['3f7e', '938c']
     print("")
-    print("bitwise AND on the above:\t\t" + str(intersected_string_array))  # ['938c']
+    print("bitwise AND on the above:\t" + str(intersected_string_array))  # ['938c']
 
 
 # Press the green button in the gutter to run the script.
--- modules/RoaringBitmapUtil.py
+++ modules/RoaringBitmapUtil.py
@@ -1,6 +1,6 @@
 from pyroaring import BitMap
-
 from modules.MD5Util import MD5Util
+import base64
 
 
 class RoaringBitmapUtil:
@@ -15,6 +15,10 @@ class RoaringBitmapUtil:
         # Create a new RoaringBitmap instance and deserialize from the byte array
         roaring_bitmap = BitMap.deserialize(byte_array)
         return roaring_bitmap
+
+    @staticmethod
+    def from_base64(base64_string):
+        return RoaringBitmapUtil.from_byte_array(base64.b64decode(base64_string))
 
     @staticmethod
     def to_string_array(bitmap):
